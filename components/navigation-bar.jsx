d578c74a0d3c7ae2afdbea8a1a9692107770e181
eeshaan <eeshaan@cs.wisc.edu>
2022-11-17 21:06:37 -0600

    add drawer

--- components/navigation-bar.jsx
+++ components/navigation-bar.jsx
@@ -2,41 +2,119 @@ import { useState } from "react";
 
 import Link from "next/link";
 
+import Icon from "./icon";
+
 /* third-party component imports */
 import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
 import Container from "@mui/material/Container";
+import Drawer from "@mui/material/Drawer";
+import Toolbar from "@mui/material/Toolbar";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 /* site data */
 import siteData from "../lib/data";
 
-export default function NavigationBar({ links, currentPage }) {
-  return (
-    <AppBar position="sticky">
-      <Container maxWidth="xl">
-        <Toolbar>
-          <Link href="/" passHref className="navbar-logo">
-            <span className="organization-name">
-              {siteData.organization.organizationName}
-            </span>{" "}
-            <span className="organization-subheading">
-              {siteData.organization.organizationSubheading}
-            </span>
-          </Link>
+const MenuIcon = ({ open, onChange }) => (
+  <div className="menu-icon">
+    <input
+      className="menu-icon__checkbox"
+      type="checkbox"
+      checked={open}
+      onChange={onChange}
+    />
+    <div>
+      <span></span>
+      <span></span>
+    </div>
+  </div>
+);
 
-          <div className="links">
-            {links.map((link) => (
-              <Link
-                href={link.link}
-                key={link.link}
-                className={"/" + currentPage === link.link ? "active" : ""}
-              >
-                {link.name}
-              </Link>
+export default function NavigationBar({ links, currentPage }) {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const showMenuIcon = useMediaQuery("(max-width: 900px)");
+
+  return (
+    <>
+      {showMenuIcon && (
+        <MenuIcon
+          open={drawerOpen}
+          onChange={() => setDrawerOpen(!drawerOpen)}
+        />
+      )}
+      <AppBar position="sticky">
+        <Container maxWidth="xl">
+          <Toolbar>
+            <Link href="/" passHref className="navbar-logo">
+              {/* <ExportedImage
+                src="/images/Effective Altruism Lightbulb.svg"
+                alt="Effective Altruism Logo"
+                width={100}
+                height={50}
+              /> */}
+              <span className="organization-name">
+                {siteData.organization.organizationName}
+              </span>{" "}
+              <span className="organization-subheading">
+                {siteData.organization.organizationSubheading}
+              </span>
+            </Link>
+
+            <div className="links">
+              {links.map((link) => (
+                <Link
+                  href={link.link}
+                  key={link.link}
+                  className={"/" + currentPage === link.link ? "active" : ""}
+                >
+                  {link.name}
+                </Link>
+              ))}
+            </div>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Drawer
+        anchor="right"
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+      >
+        <div className="drawer">
+          <ul className="links">
+            <h2>Pages</h2>
+
+            {siteData.footer.links.map((link) => (
+              <li key={link.name}>
+                <Link
+                  href={link.link}
+                  target={link.external ? "_blank" : "_self"}
+                  className={"/" + currentPage === link.link ? "active" : ""}
+                >
+                  {link.external && <Icon icon="Link" />}
+                  {link.name}
+                </Link>
+              </li>
             ))}
-          </div>
-        </Toolbar>
-      </Container>
-    </AppBar>
+          </ul>
+
+          <ul className="links">
+            <li>
+              <h2>Social</h2>
+            </li>
+
+            {siteData.social.links.map((link) => (
+              <li key={link.name}>
+                <Link
+                  href={link.link}
+                  target={link.newTab ? "_blank" : "_self"}
+                >
+                  {link.socialIcon && <Icon icon={link.socialIcon} />}
+                  {link.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Drawer>
+    </>
   );
 }
