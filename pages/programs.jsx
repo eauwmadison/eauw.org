f2bbe1a5f7f72611037afb6b94320ef29e3a851a
jkintana <jkintana@protonmail.com>
2023-02-12 17:19:16 -0600

    added comments describing functionality

--- pages/programs.jsx
+++ pages/programs.jsx
@@ -9,12 +9,28 @@ import { getCollection, getCollectionItem } from "../lib/collections";
 export default function Programs({ page, programs, popups }) {
   programs.sort((a, b) => a.priority - b.priority);
 
+  // let filter = "Animal Advocacy";
+
+  // const currentPrograms = programs.filter((program) => !program.previous && program.parentGroup === filter);
+
+  /*
+  Filter functionality:
+  - "All" displays all programs
+  - drop-down for specific programs
+    - displays on previous programs as well
+    - changes heading "current programs" to "current animal advocacy programs" etc.
+    - THIS WILL BE ON A NEW PAGE. NEEDS A SHAREABLE LINK.
+  - message or display if no programs available
+  */
+
   const currentPrograms = programs.filter((program) => !program.previous);
   const previousPrograms = programs.filter((program) => program.previous);
 
   return (
     <PageLayout page={page} popups={popups}>
-      {currentPrograms.length !== 0 && <h2>Current Programs</h2>}
+      {currentPrograms.length !== 0 && (
+        <h2>Current Programs Are Very Neat And You Should Go To Them</h2>
+      )}
       <div className="programs-grid">
         {currentPrograms.map((program, i) => (
           <Program program={program} key={i} />
